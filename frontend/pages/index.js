ca8ba8983cfc59fac637963725a611ee8d912acf
Pedro Morales <pedrojmorales01@gmail.com>
2023-05-22 13:39:28 -0700

    moved front end to fronten d/

---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import NavBar from '../components/NavBar'
+
+export default function index () {
+  return (
+    <>
+      <NavBar/>
+    </>
+    
+  )
+}
---
+++ frontend/pages/login.js
@@ -0,0 +1,12 @@
+"use client"
+
+import NavBar from '../components/NavBar'
+
+export default function Login () {
+    return (
+        <>
+            <NavBar />
+            <h1>Login</h1>
+        </>
+    )
+}
