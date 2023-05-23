2b0d7189a939f6aa716ee944b996e016137d3f7a
Pedro Morales <pmorales01@gmail.com>
2023-05-22 21:12:46 -0700

    created the User model for logging in and creating users

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
 import os 
 
 app = FastAPI()
@@ -11,7 +12,22 @@ mongo_client = AsyncIOMotorClient("mongodb://localhost:27017")
 db = mongo_client["market"]
 collection = db["products"]
 
-@app.get("/")
+# User model
+class User(BaseModel):
+    username: str = Form(...)
+    email: str = Form(...)
+    password: str = Form(...)
+
+# GET request made to the index page
+@app.get('/')
 async def index():
     return "Welcome to Market+!"
- 
+
+# POST request sent by the login form
+@app.post('/login/')
+async def login(user: User):
+    try:
+        user = User(username=user.username, email=user.email, password=user.password)
+    except:
+        return {'error': '...'}
+    return {'username': username, 'email': email, 'password' : password}
