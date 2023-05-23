9fa2bf8549a109e7734cd2c85f87ce34b88f27e0
Pedro Morales <pmorales01@gmail.com>
2023-05-22 21:09:19 -0700

    added basic login form

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -6,7 +6,26 @@ export default function Login () {
     return (
         <>
             <NavBar />
-            <h1>Login</h1>
+            <form method='post' autoComplete='on' className="form-control w-full max-w-xs">
+                <label className="label">
+                    <span className="label-text">Username</span>
+                    <span className="label-text-alt text-red-500">Required * </span>
+                </label>
+                <input type="text" name="username" className="input input-bordered input-sm w-full max-w-xs" required></input>
+                <label className="label">
+                    <span className="label-text">Email</span>
+                    <span className="label-text-alt text-red-500">Required * </span>
+                </label>
+                <input type="email" name="email" className="input input-bordered input-sm w-full max-w-xs" required></input>
+                <label className="label">
+                    <span className="label-text">Password</span>
+                    <span className="label-text-alt text-red-500">Required * </span>
+                </label>
+                <input type="password" name="password" className="input input-bordered input-sm w-full max-w-xs" required></input>
+                <button className="btn btn-primary">
+                    Submit
+                </button>
+            </form>
         </>
     )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,14 +5,24 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/colors/themes')['[data-theme=light]'], 
+          primary: '#28BFF6',
+          '.btn-primary:hover': {
+            'background-color': 'red'
+          },
+          'span': {
+            'font-size': '14px'
+          }
+        },
       },
-    },
+    ],
   },
-  plugins: [],
+
+
+  plugins: [require("daisyui")],
 }
