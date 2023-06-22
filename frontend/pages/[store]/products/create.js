651b269ce2f5203d1b2637398d7475fbebae4f32
Pedro Morales <pedrojmorales01@gmail.com>
2023-06-21 21:49:47 -0700

    fixed issue with onClick event selecting target instead of currentTarget

--- frontend/pages/[store]/products/create.js
+++ frontend/pages/[store]/products/create.js
@@ -2,19 +2,18 @@ import NavBar from '/components/NavBar'
 import Footer from '/components/Footer'
 
 export default function create_product() {
-    const toggle = (e) => {
+    const toggle = (e) => {        
         const buttons = document.getElementById('images').children
-
         // deselect all the buttons
         for (let i = 0; i < buttons.length; i++) {
-            buttons[i].classList.remove('border-black', 'border-dashed')
+            buttons[i].classList.remove('border-2', 'border-black', 'border-dashed')
         }
 
         // select the current button
-        if (e.target.classList.contains('border-dashed')) {
-            e.target.classList.remove('border-black', 'border-dashed')
+        if (e.currentTarget.classList.contains('border-dashed')) {
+            e.currentTarget.classList.remove('border-2', 'border-black', 'border-dashed')
         } else {
-            e.target.classList.add('border', 'border-black', 'border-dashed')
+            e.currentTarget.classList.add('border-2', 'border-black', 'border-dashed')
         }
     }
 
@@ -24,18 +23,20 @@ export default function create_product() {
             <div className='flex flex-row w-11/12 h-fit bg-blue-600'>
                 <div className='w-2/3 flex flex-row'>
                     <div id="images" className='flex flex-col items-center w-1/4 bg-green-200 space-y-3'>
-                        <button className='bg-base-200 w-32 h-32 border border-2 hover:border-black' onClick={toggle}>
+                        <button className='bg-base-200 w-32 h-32 border hover:border-black' onClick={toggle}>
                             <figure>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="-250 -250 1028 1028" fill='grey'>
                                     <path d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192c0 35.3 28.7 64 64 64s64-28.7 64-64H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/>
                                 </svg>
                             </figure>
                         </button>
-                        <button className='bg-base-200 w-32 h-32 border border-2 hover:border-black' onClick={toggle}>
+                        <button className='bg-base-200 w-32 h-32 border hover:border-black' onClick={toggle}>
+                        </button>
+                        <button className='bg-base-200 w-32 h-32 border hover:border-black' onClick={toggle}>
                         </button>
                     </div>
                     <div className='w-3/4 flex justify-center items-center'>
-                        <div className='bg-slate-200 w-11/12 h-11/12 border-solid border-2 border-black'>
+                        <div className='bg-slate-200 w-11/12 h-11/12 border-solid border-black'>
                         </div>
                     </div>
                 </div>
