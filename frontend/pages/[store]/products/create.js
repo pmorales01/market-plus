36c81d52af49b1a5f53348e1070e3b8fa1152b64
Pedro Morales <pedrojmorales01@gmail.com>
2023-06-24 23:47:57 -0700

    user is able to drag an image from desktop and replace it on <button>, and clicking on <button> displays an enlarged version of the image

--- frontend/pages/[store]/products/create.js
+++ frontend/pages/[store]/products/create.js
@@ -9,13 +9,47 @@ export default function create_product() {
             buttons[i].classList.remove('border-2', 'border-black', 'border-dashed')
         }
 
+        const preview = document.getElementById('image-preview')
+
+        while (preview.firstChild) {
+            preview.removeChild(preview.firstChild)
+        }
+
         // select the current button
         if (e.currentTarget.classList.contains('border-dashed')) {
             e.currentTarget.classList.remove('border-2', 'border-black', 'border-dashed')
         } else {
             e.currentTarget.classList.add('border-2', 'border-black', 'border-dashed')
+            const image = document.createElement('img')
+            image.src = e.target.src
+            preview.appendChild(image)
+            console.log(e.currentTarget)
         }
     }
+
+    const dragOverHandler = ((event) => {
+        event.preventDefault()
+    })
+
+    const dropHandler = ((event) => {
+        event.preventDefault()
+        console.log(event.dataTransfer.files[0]);
+        const file = event.dataTransfer.files[0]
+        const button = event.currentTarget
+
+        const reader = new FileReader()
+        reader.addEventListener("load", (e) => {
+            const image = document.createElement('img')
+            image.src = e.target.result
+            image.classList = 'aspect-square'
+            button.replaceChild(image, button.firstChild)
+            button.appendChild(image)
+        })
+        reader.readAsDataURL(file);
+
+        console.log(event.currentTarget)
+    })
+
 
     return (
         <div className="flex flex-col items-center space-y-14 w-full h-screen">
@@ -23,19 +57,15 @@ export default function create_product() {
             <div className='flex flex-row w-11/12 h-fit bg-blue-600'>
                 <div className='w-2/3 flex flex-row'>
                     <div id="images" className='flex flex-col items-center w-1/4 bg-green-200 space-y-3'>
-                        <button className='bg-base-200 w-32 h-32 border hover:border-black' onClick={toggle}>
-                            <figure>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="-250 -250 1028 1028" fill='grey'>
-                                    <path d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192c0 35.3 28.7 64 64 64s64-28.7 64-64H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/>
-                                </svg>
-                            </figure>
+                        <button className='bg-base-200 w-32 h-32 border hover:border-black flex justify-center' onClick={toggle} id="drop-zone" onDrop={dropHandler} onDragOver={dragOverHandler}>
+                            <img src="/svgs/upload.svg" className="aspect-square" />
                         </button>
                         <button className='bg-base-200 w-32 h-32 border hover:border-black' onClick={toggle}>
                         </button>
                         <button className='bg-base-200 w-32 h-32 border hover:border-black' onClick={toggle}>
                         </button>
                     </div>
-                    <div className='w-3/4 flex justify-center items-center'>
+                    <div className='w-3/4 flex justify-center items-center' id="image-preview">
                         <div className='bg-slate-200 w-11/12 h-11/12 border-solid border-black'>
                         </div>
                     </div>
